7d4c37fbf6c5f5d39e324f16f13b2f8eb2b87aa7
Vector Blue <modrivektor@gmail.com>
2026-02-13 02:58:35 +0100

    BubbleSort Class

--- Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/main.cpp
+++ Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/main.cpp
@@ -1,47 +1,93 @@
 #include <raylib.h>
-
+#include <stdlib.h>
 #include <time.h>
+#include <iostream>
 
+class BubbleSort{
+    private:
+        int i = 0;
+        int j = 0;
+    public: 
+        int GetSortingIndex(){
+            return j;
+        }
+        bool sorted = false;
+        void BubbleStep(int array[], int arraySize){
+            if(i >= arraySize){
+                sorted = true;
+            }
+            if (sorted) return;
+
+            if( i < arraySize - 1){
+                if(j < arraySize -i - 1){
+                    if(array[j] > array[j+1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j+1];
+                    array[j+1] = temp; 
+                    }
+                    j++;
+                } else{
+                    i ++;
+                    j = 0;
+                }
+            }
+        }
+};
+
+void RandomiseArray(int barCount, int array[], int screenHeight){
+    for(int i = 0; i < barCount; i++){
+        array[i] = GetRandomValue(0,screenHeight);
+    }
+}
 
 int main() 
 {
     
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    const int barCount = 100;
-    const Color darkBlue = {25,25,100,255};
+    const int screenWidth = 1600;
+    const int screenHeight = 1000;
+    const int barCount = 800;
+    const Color lightBlue = {150,150,255,255};
+    bool start = false;
     int array[barCount] = {0};
     int barWidth = (int)(screenWidth/barCount);
-    for(int i = 0; i < barCount; i++){
-        array[i] = GetRandomValue(0,screenHeight);
-    }
+   
+    srand(time(NULL));
 
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(60);
+    RandomiseArray(barCount, array, screenHeight);
     
+    InitWindow(screenWidth, screenHeight, "Bubble Sort Visualiser");
+    
+    BubbleSort bubbleSort;
+
+    while (!WindowShouldClose() && !start){
+        BeginDrawing();
+        if(IsKeyPressed(KEY_SPACE)){
+            start = true;
+        }
+        for(int i = 0; i < barCount; i++){
+            DrawRectangle(i*barWidth, screenHeight-array[i], barWidth, array[i], WHITE);
+        }
+        EndDrawing();
+    }
     while (!WindowShouldClose())
     {
-        
+        bubbleSort.BubbleStep(array, barCount);
         BeginDrawing();
         ClearBackground(BLACK);
         for(int i = 0; i < barCount; i++){
-            DrawRectangle(i*barWidth, 0, barWidth, array[i], WHITE);
+            DrawRectangle(i*barWidth, screenHeight-array[i], barWidth, array[i], WHITE);
         }
-        for(int i = 0; i < barCount; i++){
-            for(int j = 0; j < barCount -i; j++){
-                DrawRectangle(i*barWidth, 0, barWidth, array[i], darkBlue);
-                WaitTime(.1);
-                if(array[j] > array[j+1]){
-                    int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
-                }
-                DrawRectangle(i*barWidth, 0, barWidth, array[i], WHITE);
-            }
-        }
+        int blueIndex = bubbleSort.GetSortingIndex();
+        DrawRectangle(blueIndex*barWidth, screenHeight-array[blueIndex], barWidth*2, array[blueIndex], lightBlue);
+        std::string fps = std::to_string(GetFPS());
+        DrawText(fps.c_str(), 0,0,20,lightBlue);
         EndDrawing();
+        
     }
     
     CloseWindow();
 }
+
+
 
